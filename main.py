6ff04796494c0564611a5621965cf89bd7b19fb7
rip6som <gyrygraf@gmail.com>
2025-11-30 12:59:51 +0100

    Add gunicorn

--- main.py
+++ main.py
@@ -10,9 +10,14 @@ app = fl.Flask(__name__)
 @app.route('/')
 def index():
     response = requests.get('https://api.github.com/users/rip6som/repos')
-    repos = response.json()
-    return render_template('index.html', repos=repos)
+    print(f"GitHub API status: {response.status_code}")
+    print(f"GitHub API response: {response.json()}")
 
+    if response.status_code == 200:
+        repos = response.json()
+    else:
+        repos = []
+    return render_template('index.html', repos=repos)
 #===========| Конец |===========#
 if __name__ == '__main__':
     app.run(debug=True)
